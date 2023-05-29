e58bdd5522be902fca3641d318081d9d577a6695
khushbu <khushbu@lakebrains.in>
2023-05-29 16:28:14 +0530

    structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Signin from './Components/AuthenticationPage/Signin';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Signin />
+    </>
   );
 }
 
---
+++ src/Components/AuthenticationPage/Signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Signin.css';
+
+function Signin() {
+    return (
+        <div className={styles.Authentication_Page_Wrapper}>
+            hi
+        </div>
+    );
+}
+
+export default Signin;
