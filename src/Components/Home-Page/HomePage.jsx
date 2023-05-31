4819e179e54a12188e669f6c81cfc2d51c5da2c0
khushbu <khushbu@lakebrains.in>
2023-05-31 14:30:22 +0530

    sidebar

--- src/Components/Home-Page/HomePage.jsx
+++ src/Components/Home-Page/HomePage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import CommanCss from '../CommanComponent/Comman.module.css'
 import Navbar from '../Navigation/Navbar/Navbar'
+import Sidebar from '../Navigation/SideBar/Sidebar'
+import { Grid } from '@mui/material'
 
 function HomePage() {
 
     return (
         <>
             <Navbar />
+            <Grid container className={CommanCss.Wrapper}>
+                <Grid item xs={1.7}>
+                    <div className={CommanCss.SidebarWrapper}>
+                        <Sidebar />
+                    </div>
+                </Grid >
+                <Grid item xs={10.3}>
+
+                </Grid >
+            </Grid>
         </>
     )
 }
