bc63020955de8df8446b7171c4b7de7fb704123a
Jitendra Singh <jitendrasinghrao9587@gmail.com>
2023-06-01 11:38:19 +0530

    edit user frontend

--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CommanCss from '../CommanComponent/Comman.module.css'
 import styles from './ProfilePage.module.css'
 import Navbar from '../Navigation/Navbar/Navbar'
-import { Grid, TextField } from '@mui/material'
+import { Box, Button, Grid, Modal, TextField } from '@mui/material'
 import Sidebar from '../Navigation/SideBar/Sidebar'
 import { UserNavData } from '../Routers/AuthRoutesWrapper'
 import editimage from '../Images/Comman/Edit.svg'
 import EditPencil from '../Images/Comman/EditPencil.svg'
 import userimage from '../Images/Comman/user.svg'
+import closeimage from '../Images/Comman/Close button.svg'
 
 
 function ProfilePage() {
+    const [open, setOpen] = useState(true)
     // useContext for navbar data
     const { userdata } = useContext(UserNavData)
+
+    // Modal Close Function
+    const handleClose = () => {
+        setOpen(false)
+    }
     return (
         <>
             <Navbar />
@@ -57,6 +64,31 @@ function ProfilePage() {
                     </>
                 </Grid >
             </Grid>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box className={styles.modalBox}>
+                    <div className={styles.modalheaderWrappper}>
+                        <h2>Edit Details</h2>
+                        <div onClick={handleClose} className={styles.closeIcon}><img src={closeimage} alt="close" /></div>
+                    </div>
+                    <div className={styles.ModalInputWrapper}>
+                        <div className={styles.UserinputModal}>
+                            <TextField className={styles.UserDisableInputFiled} value={userdata.name} label="Name" variant="outlined" type='email' fullWidth required />
+                        </div>
+                        <div className={styles.UserinputModal}>
+                            <TextField className={styles.UserDisableInputFiled} value={userdata.email} label="Email" variant="outlined" type='email' fullWidth required />
+                        </div>
+                    </div>
+                    <div className={styles.Footerbtns}>
+                        <Button className={styles.footerbtnupdate}>Update</Button>
+                        <Button className={styles.footerbtncancel}>Cancel</Button>
+                    </div>
+                </Box>
+            </Modal>
         </>
     )
 }
